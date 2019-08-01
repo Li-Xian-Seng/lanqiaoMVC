10af93b97b688a8c859cb72226b9a829c3070e44
liyupeng <2575566766@qq.com>
2019-08-01 15:24:18 +0800

    加入用户类

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,71 @@
+package entity;
+
+public class User {
+private int id;
+    private String username;
+    private String password;
+    private int status;
+    private String images_header;
+    private String staffname;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int deptId;
+
+    public int getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(int deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getStaffname() {
+        return staffname;
+    }
+
+    public void setStaffname(String staffname) {
+        this.staffname = staffname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getImages_header() {
+        return images_header;
+    }
+
+    public void setImages_header(String images_header) {
+        this.images_header = images_header;
+    }
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
