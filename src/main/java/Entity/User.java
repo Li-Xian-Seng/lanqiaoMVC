e7d04f64eb38ac66ca33e1d45df90b1efd3ba2c5
liyupeng <2575566766@qq.com>
2019-08-02 20:29:39 +0800

    关于用户的dao类

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,68 @@
+package Entity;
+
+public class User {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getImages_header() {
+        return images_header;
+    }
+
+    public void setImages_header(String images_header) {
+        this.images_header = images_header;
+    }
+
+    public String getStaffname() {
+        return staffname;
+    }
+
+    public void setStaffname(String staffname) {
+        this.staffname = staffname;
+    }
+
+    public int getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(int deptId) {
+        this.deptId = deptId;
+    }
+
+    private String username;
+    private String password;
+    private int status;
+    private String images_header;
+    private String staffname;
+    private int deptId;
+}
