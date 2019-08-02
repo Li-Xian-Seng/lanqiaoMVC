5ffbc23a37b036a51ab9776ae264fd2e1db108ea
liyupeng <2575566766@qq.com>
2019-08-02 21:08:55 +0800

    只能删除权限为用户的用户

--- src/main/java/dao/DeleteDao.java
+++ src/main/java/dao/DeleteDao.java
@@ -4,7 +4,7 @@ import util.DButil;
 
 public class DeleteDao {
     public int deleteDao(int id){
-        String sql="UPDATE t_user SET status = 0 WHERE id=? ";
+        String sql="UPDATE t_user a INNER JOIN t_user_role c on c.user_id=a.id SET a.status = 0 WHERE c.role_id=3 and id=？ ";
         int i=DButil.update(sql,id);
         return i;
     }
