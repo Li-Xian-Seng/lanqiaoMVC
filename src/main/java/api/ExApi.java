57e9d2979b719a2d439e5a41801c68277ebbc930
liyupeng <2575566766@qq.com>
2019-08-02 20:19:50 +0800

    查询用户的接口

--- src/main/java/api/ExApi.java
+++ src/main/java/api/ExApi.java
@@ -1,4 +1,37 @@
 package api;
 
+import Entity.UserRole;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.UserService;
+import vo.JsonRequst;
+
+import java.util.List;
+
+@Controller
 public class ExApi {
+    /**
+     * 查询用户信息角色 和部门
+     * @return
+     */
+    @RequestMapping(value = "/api/images/quary",method = RequestMethod.GET)
+    @ResponseBody
+    public JsonRequst FQuarys(){
+        UserService service=new UserService();
+        List<UserRole> user=service.userService();
+        JsonRequst json=null;
+        try{
+            if(user!=null){
+                json=new JsonRequst("200","查询成功",user);
+            }else {
+                json=new JsonRequst("404","未查到用户",null);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            json=new JsonRequst("500","查询异常",e.getMessage());
+        }
+        return  json;
+    }
 }
