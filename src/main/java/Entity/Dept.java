1d1b8d7c64e84ee6e9ee0c91f9f5cdcfb377ae5e
liyupeng <2575566766@qq.com>
2019-08-16 14:18:49 +0800

    更新

---
+++ src/main/java/Entity/Dept.java
@@ -0,0 +1,39 @@
+package entity;
+
+public class Dept {
+    private int id;
+    private int code;
+    private String dept_name;
+    private int parantid;
+    public int getId() {
+        return id;
+    }
+
+    public int getParantid() {
+        return parantid;
+    }
+    public void setParantid(int parantid) {
+        this.parantid = parantid;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDept_name() {
+        return dept_name;
+    }
+
+    public void setDept_name(String dept_name) {
+        this.dept_name = dept_name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+}
